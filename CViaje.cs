dab1f92550bcd6eb181645d96a531e7f66db8e23
Gerald <geraldjoly191@gmail.com>
2024-10-22 22:51:27 -0300

    First commit

---
+++ CViaje.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_de_viajes
+{
+    public class CViaje
+    {
+        static float costoPorKilometro;
+        static uint kilometrajeMinimo;
+        private float peaje;
+        private uint distanciaRecorida;
+        private string dominio;
+
+
+        public CViaje() { this.dominio = "XX XXX XXX"; }
+        public CViaje(string dominio,uint distanciaRecorida)
+        {
+            this.dominio = dominio;
+            this.distanciaRecorida = distanciaRecorida;
+        }
+
+
+        public void setCostoporKilomentro(float costo) { CViaje.costoPorKilometro = costo; }
+        public void setKilometrajeMinimo(uint kilometrajes) { CViaje.kilometrajeMinimo= kilometrajes; }
+        public void setDominio(string dominio) {  this.dominio = dominio;}
+
+        public float getCostoPorKilometro() { return costoPorKilometro;}
+        public uint getKilometrajeMinimo() { return CViaje.kilometrajeMinimo; }
+        public uint getDistanciaRecorida() {  return distanciaRecorida;}
+        public string getDominio() {  return this.dominio;}
+
+        public uint Kilometraje
+        {
+            get => distanciaRecorida;
+            set => distanciaRecorida = value;
+        }
+
+
+        public float DarCosto()
+        {
+            if(distanciaRecorida < kilometrajeMinimo)
+            {
+                return kilometrajeMinimo * costoPorKilometro;
+            }
+
+            return distanciaRecorida* costoPorKilometro;
+        }
+
+        public float DarCosto( float peaje)
+        {
+            return DarCosto()*peaje;
+        }
+
+        public int MayorViaje(CViaje otro)
+        {
+            int mayor = (int)(this.distanciaRecorida - otro.getDistanciaRecorida());
+
+            if(mayor < 0) { return -1; }
+            else if(mayor == 0) { return 0; }
+
+
+              return 1; 
+        }
+        public string DarDatos()
+        {
+            return "\n Dominio :" + this.dominio + "\n Costo por Kilometro :"+ CViaje.costoPorKilometro + "\n Distancia Recorida :" + this.distanciaRecorida +"\n Kilomentraje minima minima :" + CViaje.kilometrajeMinimo;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Gestion_de_viajes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+         float costoPorKilometro,peaje;
+         uint kilometrajeMinimo, distanciaRecorida,cont=0;
+         string dominio;
+
+            CViaje viaje, viajeMax=null;
+
+            Console.WriteLine("Ingrese El costo por kilometro");
+            costoPorKilometro = float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Ingrese el kilometraje minimo");
+            kilometrajeMinimo = uint.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el Dominio del Auto");
+            dominio = Console.ReadLine().ToUpper();
+
+            while (dominio != "FIN")
+            {
+
+                Console.WriteLine("Ingrese la distancia recorida por el auto");
+                distanciaRecorida= uint.Parse(Console.ReadLine());
+
+                viaje = new CViaje(dominio,distanciaRecorida);
+
+                if(cont == 0)
+                {
+                    viajeMax= viaje;
+                }
+
+                if (viaje.MayorViaje(viajeMax) == 1) { viajeMax = viaje; }
+
+                cont++;
+                Console.WriteLine("Ingrese el Dominio del Auto");
+                dominio = Console.ReadLine().ToUpper();
+            }
+
+
+            if(cont == 0)
+            {
+                Console.WriteLine("No se ingreso  un valor valido");
+            }
+            else
+            {
+                Console.WriteLine($"El mayor viaje es {viajeMax.DarDatos()}");
+            }
+         }
+    }
+}
